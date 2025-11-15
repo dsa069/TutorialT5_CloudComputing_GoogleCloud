38ca973663ccda6e606b50bd6f42a5eb02902f17
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2025-11-15 21:43:26 +0100

    infraestructura gc

--- main.tf
+++ main.tf
@@ -1,115 +1,136 @@
-# Crear red desarrollo-net
-resource "openstack_networking_network_v2" "desarrollo_net" {
-  name           = "desarrollo-net"
-  admin_state_up = "true"
+resource "google_compute_network" "vpc_network" {
+  name = var.gcp-network
+  auto_create_subnetworks = false
 }
 
-# Crear subred desarrollo-subnet
-resource "openstack_networking_subnet_v2" "desarrollo_subnet" {
-  name       = "desarrollo-subnet"
-  network_id = openstack_networking_network_v2.desarrollo_net.id
-  cidr       = "10.2.0.0/24"
-  ip_version = 4
-  dns_nameservers = ["150.214.156.2", "8.8.8.8"]
-}
+resource "google_compute_firewall" "firewall-icmp" {
+  name    = "terraform-allow-icmp"
+  network = google_compute_network.vpc_network.name
 
-# Crear router desarrollo-router
-data "openstack_networking_network_v2" "public" {
-  name = "public1"
-}
-
-resource "openstack_networking_router_v2" "desarrollo_router" {
-  name                = "desarrollo-router"
-  admin_state_up      = "true"
-  external_network_id = data.openstack_networking_network_v2.public.id
-}
-
-# Conectar el router a la subred
-resource "openstack_networking_router_interface_v2" "router_interface" {
-  router_id = openstack_networking_router_v2.desarrollo_router.id
-  subnet_id = openstack_networking_subnet_v2.desarrollo_subnet.id
-}
-
-#Crear nodo tf_vm
-#Creación de un recurso instancia (máquina virtual) en OpenStack. El objeto recurso creado es asignado a la variable tf_vm.
-resource "openstack_compute_instance_v2" "tf_vm" {
-  name              = "tf_vm"
-  image_name        = "ubuntu24.04"
-  availability_zone = "nova"
-  flavor_name       = "m1.large"
-  key_pair          = var.openstack_keypair
-  security_groups   = var.openstack_security_groups
-  network {
-    #Red a la que se conectará la instancia creada. Usamos una variable de entrada almacenada en variables.tf con el nombre de la red.
-    uuid = openstack_networking_network_v2.desarrollo_net.id
-
-  }
-  user_data = file("install_mysql.sh")
-}
-
-#Crear nodo appserver
-resource "openstack_compute_instance_v2" "appserver" {
-  name              = "appserver"
-  image_name        = "ubuntu24.04"
-  availability_zone = "nova"
-  flavor_name       = "m1.medium"
-  key_pair          = var.openstack_keypair
-  security_groups = var.openstack_security_groups
-
-  network {
-    uuid = openstack_networking_network_v2.desarrollo_net.id
+  allow {
+    protocol = "icmp"
   }
 
-    user_data = templatefile("${path.module}/install_appserver.tpl", { mysql_ip = openstack_compute_instance_v2.tf_vm.network.0.fixed_ip_v4 })
-
-  depends_on = [openstack_compute_instance_v2.tf_vm]
-
+  source_ranges = ["0.0.0.0/0"]
 }
 
-#Creación de un recurso dirección IP flotante. El objeto recurso creado es asignado a la variable tf_vm_ip.
-resource "openstack_networking_floatingip_v2" "tf_vm_ip" {
-  pool = "public1"
+resource "google_compute_firewall" "firewall-ssh" {
+  name    = "terraform-allow-ssh"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
 }
 
-# Crear floating IP para appserver
-resource "openstack_networking_floatingip_v2" "appserver_ip" {
-  pool = "public1"
+# allow http traffic
+resource "google_compute_firewall" "allow-http" {
+  name    = "tf-fw-allow-http"
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+  target_tags   = ["http"]
+  source_ranges = ["0.0.0.0/0"]
 }
 
-#Asociación de la IP flotante a la instancia
-#Acceso a la dirección del recurso IP flotante creado
-#Acceso al id la instancia creada
-resource "openstack_compute_floatingip_associate_v2" "tf_vm_ip" {
-  floating_ip = openstack_networking_floatingip_v2.tf_vm_ip.address
-  instance_id = openstack_compute_instance_v2.tf_vm.id
+resource "google_compute_firewall" "firewall-internal" {
+  name    = "terraform-allow-internal"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports    = ["0-65535"]
+  }
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = ["10.128.0.0/20"]
 }
 
-#Acceso a la dirección del recurso IP flotante creado
-#Esperar a que esté creado el recurso de la IP flotante
-output tf_vm_Floating_IP {
-  value      = openstack_networking_floatingip_v2.tf_vm_ip.address
-  depends_on = [openstack_networking_floatingip_v2.tf_vm_ip]
+#Crear subnet
+resource "google_compute_subnetwork" "subnet" {
+  name          = "terraform-subnet"
+  ip_cidr_range = "10.0.0.0/20"
+  region        = "us-central1"
+  network       = google_compute_network.vpc_network.id
 }
 
-resource "openstack_compute_floatingip_associate_v2" "appserver_ip" {
-  floating_ip = openstack_networking_floatingip_v2.appserver_ip.address
-  instance_id = openstack_compute_instance_v2.appserver.id
+#Crear una IP estática reservada
+resource "google_compute_address" "tf-vm-ip" {
+  name = "ipv4-address-tf-vm"
 }
 
-output appserver_Floating_IP {
-  value      = openstack_networking_floatingip_v2.appserver_ip.address
-  depends_on = [openstack_networking_floatingip_v2.appserver_ip]
+resource "google_compute_instance" "tf-vm" {
+  name         = "tf-vm"
+  zone         = "us-central1-c"
+  machine_type = "n1-standard-2"
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+    }
+  }
+
+  # Add SSH access to the Compute Engine instance
+  metadata = {
+    ssh-keys = "${var.gcp-username}:${file("~/.ssh/clave-gc.pub")}"
+  }
+
+  # Add http tag to the instance to identify it in the firewall rule
+  tags = ["http"] 
+
+  # Startup script
+  metadata_startup_script = file("setup-docker.sh")
+
+  network_interface {
+    network    = var.gcp-network
+    subnetwork = google_compute_subnetwork.subnet.name
+
+    # Se comenta el access_config vacío (IP efímera) porque se usará una IP estática
+    # access_config {}
+
+    # Asignar la IP estática reservada creada anteriormente
+    access_config {
+      nat_ip = google_compute_address.tf-vm-ip.address
+    }
+
+  }
 }
 
-#Crear volumen 1GB
-resource "openstack_blockstorage_volume_v3" "tf_vol" {
-  name        = "tf_vol"
-  description = "first test volume"
-  size        = 1
+output "tf-vm-ip" {
+  value      = google_compute_address.tf-vm-ip.address
+  depends_on = [google_compute_instance.tf-vm]
 }
 
-#Adjuntar volumen a la instancia
-resource "openstack_compute_volume_attach_v2" "va_1" {
-  instance_id = "${openstack_compute_instance_v2.tf_vm.id}"
-  volume_id   = "${openstack_blockstorage_volume_v3.tf_vol.id}"
+resource "google_compute_disk" "tf-disk" {
+  name = "tf-disk"
+  type = "pd-ssd"
+  size = 1
 }
+
+resource "google_compute_attached_disk" "attached-tf-disk" {
+  disk     = google_compute_disk.tf-disk.id
+  instance = google_compute_instance.tf-vm.id
+}
+
+output "tf-vm-internal-ip" {
+  value      = google_compute_instance.tf-vm.network_interface.0.network_ip
+  depends_on = [google_compute_instance.tf-vm]
+}
+
+# Output de IP efímera comentado porque ahora se usa la IP estática
+# output "tf-vm-ephemeral-ip" {
+#   value      = google_compute_instance.tf-vm.network_interface.0.access_config.0.nat_ip
+#   depends_on = [google_compute_instance.tf-vm]
+# }
